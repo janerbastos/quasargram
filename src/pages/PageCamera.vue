792379d7e7537065ebeb4b1bc6e9ae927d5c6034
Janer B M <janerbastos@gmail.com>
2024-04-16 22:54:45 -0300

    Native Device Features - Camera

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,16 +1,40 @@
 <template>
   <q-page class="constrain-more q-pa-md">
     <div class="camera-frame q-pa-md">
-      <img
+      <video
+        v-show="!imageCaptured"
+        ref="video"
         class="full-width"
-        src="https://cdn.quasar.dev/img/parallax2.jpg" >
+        autoplay
+      />
+      <canvas
+        v-show="imageCaptured"
+        ref="canvas"
+        class="full-width"
+        height="240"
+      />
     </div>
     <div class="text-center q-pa-md">
       <q-btn
+        v-if="hasCameraSupport"
+        @click="captureImage"
         round 
         color="black"
         size="lg"
         icon="eva-camera" />
+
+        <q-file
+          v-else
+          v-model="imageUpload"
+          label="Choose an image"
+          accept=" image/*"
+          @input="captureImageFallback"
+          outlined
+        >
+          <template v-slot:prepend>
+            <q-icon name="eva-attach-outline" />
+          </template>
+        </q-file>
     </div>
     <div class="row justify-center q-ma-md">
       <q-input 
@@ -46,6 +70,8 @@
 
 <script>
 import { uid } from 'quasar'
+require('md-gum-polyfill')
+
 export default {
   name: 'PageCamera',
   data() {
@@ -56,7 +82,76 @@ export default {
         location: "",
         date: Date.now(),
         photo: null,
+      },
+      imageCaptured: false,
+      imageUpload: [],
+      hasCameraSupport: true,
+    }
+  },
+  methods: {
+    initCamera(){
+      navigator.mediaDevices.getUserMedia({
+        video: true
+      }).then(strean => {
+        this.$refs.video.srcObject = strean
+      }).catch( error => {
+        this.hasCameraSupport = false
+      })
+    },
+    captureImage() {
+      let video = this.$refs.video
+      let canvas = this.$refs.canvas
+      canvas.width = video.getBoundingClientRect().width
+      canvas.height = video.getBoundingClientRect().height
+      let context = canvas.getContext('2d')
+      context.drawImage(video, 0, 0, canvas.width, canvas.height)
+      this.imageCaptured = true
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL())
+      this.disableCamera()
+    },
+
+    captureImageFallback( file ) {
+      this.post.photo = file
+      let canvas = this.$refs.canvas
+      let context = canvas.getContext('2d')
+      var reader = new FileReader()
+      reader.onload = (event) => {
+          var img = new Image()
+          img.onload = () => {
+            canvas.width = img.width
+            canvas.height = img.height
+            context.drawImage(img, 0, 0)
+            this.imageCaptured = true
+          }
+          img.src = event.target.result
       }
+      reader.readAsDataURL(file)
+    },
+
+    disableCamera() {
+      this.$refs.video.srcObject.getVideoTracks().forEach(track => {
+        track.stop()
+      })
+    },
+
+    dataURItoBlob(dataURI) {
+      var byteString = atob(dataURI.split(',')[1]);
+      var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+      var ab = new ArrayBuffer(byteString.length);
+      var ia = new Uint8Array(ab);
+      for (var i = 0; i < byteString.length; i++) {
+          ia[i] = byteString.charCodeAt(i)
+      }
+      var blob = new Blob([ab], {type: mimeString})
+      return blob;
+    }
+  },
+  mounted() {
+    this.initCamera()
+  },
+  beforeDestroy() {
+    if(this.hasCameraSupport){
+      this.disableCamera()
     }
   }
 }
